e06724c76385c4fd7d2e56d7934769fcf48ec0de
k.osawa <saataysu4408999@gmail.com>
2024-12-13 16:08:46 +0900

    added auth.guard.ts auth.service.ts and update app.routes.ts

--- projects/signal-ex05/src/app/app.component.ts
+++ projects/signal-ex05/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from './sevices/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'signal-ex05';
+
+  authService = inject(AuthService);
+
+  isLoggedIn = this.authService.isLoggedIn;
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
--- projects/signal-ex05/src/app/app.routes.ts
+++ projects/signal-ex05/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { isUserAuthenticated } from './guards/auth.guard';
 
 export const routes: Routes = [
     {
       path: '',
       component: HomeComponent,
-     
+      canActivate: [isUserAuthenticated]     
     },
     {
       path: "login",
